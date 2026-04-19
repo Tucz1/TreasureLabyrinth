d4c7f9d057e162e2d8d4a56704fd49e002e634b9
Jaakko Mansikka <mansikkajaakko13@gmail.com>
2026-04-19 16:43:18 +0300

    AudioManager

---
+++ TreasureLabyrinth/Assets/Workspaces/Jaakko/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,42 @@
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager I {get;private set;}
+
+    [Header("Mixer")]
+    [SerializeField] private AudioMixer m_mainMixer;
+
+    private void Awake()
+    {
+        if (I != null) 
+        {
+            Destroy(gameObject);
+            return;
+        }
+        I = this;
+        DontDestroyOnLoad(this);
+
+        UIEvents.OnMusicVolumeChanged += MusicVolumeChanged;
+        UIEvents.OnSfxVolumeChanged += SfxVolumeChanged;
+    }
+    private void OnDestroy()
+    {
+        if (I != this) 
+        {
+            return;
+        }
+        UIEvents.OnMusicVolumeChanged -= MusicVolumeChanged;
+        UIEvents.OnSfxVolumeChanged -= SfxVolumeChanged;
+    }
+    private void MusicVolumeChanged(float value) 
+    {
+        m_mainMixer.SetFloat("MusicVolume", value);   
+    }
+    private void SfxVolumeChanged(float value) 
+    {
+        m_mainMixer.SetFloat("SfxVolume", value);
+    }
+}
