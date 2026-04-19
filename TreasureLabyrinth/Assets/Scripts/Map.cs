1979f989713f5c9ba73b7866467c95c57bcfebd8
Nicolas <nicolas.piirainen@gmail.com>
2026-04-19 14:52:44 +0300

    Added color reading and selection for tiles

--- TreasureLabyrinth/Assets/Scripts/Map.cs
+++ TreasureLabyrinth/Assets/Scripts/Map.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using NUnit.Framework;
 using UnityEngine;
 
 public enum TileType
@@ -32,8 +34,9 @@ public class Node
 [System.Serializable]
 public class MapDataColors
 {
-    public ColorRole colorRole;
+    public string colorRole;
     public Color32 color;
+
 }
 
 public class Map : MonoBehaviour
@@ -42,6 +45,9 @@ public class Map : MonoBehaviour
     public Dictionary<Vector2Int, Node> data;
 
     public MapDataColors[] mapColors;
+    [SerializeField] int colorThreshold = 20;
+
+    public Dictionary<string, Color32> mapDataColors;
 
     private int mapSizeX;
     private int mapSizeY;
@@ -49,18 +55,29 @@ public class Map : MonoBehaviour
     public GameObject testWall;
     public GameObject testFloor;
     public Transform levelVisuals;
-    
+
     public Texture2D testingTex;
 
-    
+
+    void Awake()
+    {
+        mapDataColors = new Dictionary<string, Color32>();
+
+        for (int i = 0; i < mapColors.Length; i++)
+        {
+            mapDataColors.Add(mapColors[i].colorRole, mapColors[i].color);
+        }
+    }
+
     void Start()
     {
+
         LoadFromTexture(testingTex);
         RefreshVisuals();
     }
 
     public void RefreshVisuals()
-    {   
+    {
         // Kill previous visuals????
 
         foreach (var thing in data)
@@ -93,7 +110,31 @@ public class Map : MonoBehaviour
             for (int j = 0; j < mapSizeY; j++)
             {
                 var pixel = pixels[i + j * mapSizeX];
-                var tileType = pixel.r > 128 ? TileType.Floor : TileType.Wall;
+                TileType tileType = TileType.None;
+
+                bool IsCloseEnough(Color32 c1, Color32 c2, int threshold)
+                {
+                    return Mathf.Abs(c1.r - c2.r) < threshold &&
+                            Mathf.Abs(c1.g - c2.g) < threshold &&
+                            Mathf.Abs(c1.b - c2.b) < threshold;
+                }
+                
+                if (IsCloseEnough(pixel, mapDataColors["Floor"], colorThreshold))
+                {
+                    // dodo
+                    tileType = TileType.Floor;
+                }
+                else if (IsCloseEnough(pixel, mapDataColors["Wall"], colorThreshold))
+                {
+                    tileType = TileType.Wall;
+                }
+
+
+
+
+                // else { tileType = pixel.r > 128 ? TileType.Floor : TileType.Wall; }
+
+                if (tileType == TileType.None) Debug.LogError("vevybad");
 
                 data.Add(new Vector2Int(i, j), new Node(tileType));
             }
