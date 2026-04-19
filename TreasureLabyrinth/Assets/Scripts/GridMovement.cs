13e79b736b765449b03ff277b993a83b6446a694
Nicolas <nicolas.piirainen@gmail.com>
2026-04-19 11:18:17 +0300

    Added basic grid based movement

---
+++ TreasureLabyrinth/Assets/Scripts/GridMovement.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using UnityEngine;
+
+public class GridMovement : MonoBehaviour
+{
+    Map map;
+    private bool isMoving;
+    [SerializeField] float moveSpeed = 1f;
+    public Vector2Int currentGridPos;
+
+    void Awake()
+    {
+        map = FindAnyObjectByType<Map>();
+
+        transform.position = new Vector3(currentGridPos.x, currentGridPos.y, -1f);
+    }
+    void Update()
+    {
+        if (isMoving) return;
+
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        if (x != 0 || y != 0)
+        {
+            Vector2Int direction = new Vector2Int((int)x, (int)y);
+
+            if (direction.x != 0) direction.y = 0;
+
+            Vector2Int targetGridPos = currentGridPos + direction;
+
+            Node targetNode = map.data[targetGridPos];
+
+            if (targetNode != null && targetNode.tileType == TileType.Floor)
+            {
+                currentGridPos = targetGridPos;
+                StartCoroutine(SmoothMove(targetGridPos));
+
+            }
+        }
+    }
+
+    IEnumerator SmoothMove(Vector2Int targetGridPos)
+    {
+        isMoving = true;
+
+        Vector3 targetWorldPos = new Vector3(targetGridPos.x, targetGridPos.y, 0f);
+
+        while (Vector3.Distance(transform.position, targetWorldPos) > 0.01f)
+        {
+            transform.position = Vector3.MoveTowards(
+                transform.position,
+                targetWorldPos,
+                moveSpeed * Time.deltaTime
+            );
+
+            yield return null;
+        }
+
+        transform.position = targetWorldPos;
+
+        isMoving = false;
+    }
+
+}
